a640890410f1c5d83730223ed196fb917cfe8499
pickleCucumber <63567038+pickleCucumber@users.noreply.github.com>
2024-04-24 22:57:48 +0300

    Update servo.py

--- servo.py
+++ servo.py
@@ -2,15 +2,29 @@ from __future__ import division
 import time
 import math
 from adafruit_servokit import ServoKit
+import mpu6050
+import numpy as np
+
+#задаем адресс объектов
+##mpu6050 = mpu6050.mpu6050(0x68)
 kit = ServoKit(channels=16)
 
+#читаем данные с датчика
+def read_sensor_data():
+    accelerometer_data = mpu6050.get_accel_data()
+    gyroscope_data = mpu6050.get_gyro_data()
+    temperature = mpu6050.get_temp()
+
+    return accelerometer_data, gyroscope_data, temperature
+
+#дальше описываем движения павука
 
 def Joint_1(A):
-    kit.servo[10].angle = A
+    kit.servo[10].angle = 180-A
 def Joint_2(A):
     kit.servo[5].angle = A
 def Joint_3(A):
-    kit.servo[2].angle = A
+    kit.servo[2].angle = 180-A
 def Joint_4(A):
     kit.servo[13].angle = A
     
@@ -77,51 +91,105 @@ def Sit():
     Calfs(30)
     time.sleep(1)
     Thighs(30)
-    
-#проходочка
-def Forward():
-    
-    Calf_1(45)    
-    Calf_3(45)
 
+
+def Left():
+    Calf_4(90)
+    Calf_1(90)
      
-    Thigh_1(30) 
-    Thigh_3(30)
-    Joint_1(90)
+    Thigh_1(120) 
+    Thigh_3(110) #желательно заменить на 120
+    Joint_1(90) 
     Joint_3(90)
     time.sleep(0.2)
     Joint_2(120) 
-    Joint_4(60)
+    Joint_4(120)
     time.sleep(0.2)
-    Calf_1(90)
-    Thigh_1(90) 
+    Thigh_1(100) 
     Thigh_3(90)
-    Calf_3(90)
+
     time.sleep(0.2)
-    Calf_2(45)
-    
-    Thigh_2(45)
-    Calf_4(45)
-    Thigh_4(45)
+    Thigh_2(120) 
+    Thigh_4(120)
     Joint_2(90) 
     Joint_4(90)
+    time.sleep(0.2)
     Joint_1(60) 
     Joint_3(60)
-    time.sleep(0.2)
-    Calf_2(90)
+    time.sleep(0.3)
     Thigh_2(90) 
     Thigh_4(90)
-    Calf_4(90)
 
     time.sleep(0.2)
  
+def Right():
+    Calf_4(90)
+    Calf_1(90)
+     
+    Thigh_1(120) 
+    Thigh_3(110) #желательно заменить на 120
+    Joint_1(90) 
+    Joint_3(90)
+    time.sleep(0.2)
+    Joint_2(60) 
+    Joint_4(60)
+    time.sleep(0.2)
+    Thigh_1(100) 
+    Thigh_3(90)
 
+    time.sleep(0.2)
+    Thigh_2(120) 
+    Thigh_4(120)
+    Joint_2(90) 
+    Joint_4(90)
+    time.sleep(0.2)
+    Joint_1(120) 
+    Joint_3(120)
+    time.sleep(0.3)
+    Thigh_2(90) 
+    Thigh_4(90)
+    time.sleep(0.2)
+    
+    
+    
+def Forward():
+    
+
+     
+    Thigh_1(120) #приподнимаем 1 и 3
+    Thigh_3(110) #желательно заменить на 120
+    Joint_1(90) 
+    Joint_3(90)
+    time.sleep(0.2)
+    Joint_2(120) #делаем Х 2 и 4 
+    Joint_4(60)
+    time.sleep(0.2)
+    Thigh_1(100) #опускаем 1 и 3
+    Thigh_3(90)
+
+    time.sleep(0.2)
+    #Обраточка
+    Thigh_2(120) #
+    Thigh_4(120)
+    Joint_2(90) #фиксируем 2 и 4
+    Joint_4(90)
+    time.sleep(0.2)
+    Joint_1(120) #делаем Х 1 и 3
+    Joint_3(60)
+    time.sleep(0.3)
+    Thigh_2(90) #опускаем 2 и 4 ногу 
+    Thigh_4(90)
+    time.sleep(0.2)    
+    
 
 
 SetUp()
-LegsUp()
-time.sleep(2)
-Sit()
-time.sleep(2)
-while True:
+time.sleep(1)
+for i in range(10):
     Forward()
+
+
+
+    #accelerometer_data, gyroscope_data, temperature = read_sensor_data()
+    #print('куда я? ', accelerometer_data, 'хде я? ', gyroscope_data, 'как обстановочка?', temperature)
+    #time.sleep(1)
